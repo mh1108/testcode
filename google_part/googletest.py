3c7e95272bbad627ace75022f6cd296f5f6d7687
denkerm <denker.m@husky.neu.edu>
2014-11-08 17:53:03 -0500

    BUG
    We have a bug where it won't check the entire list before breaking

--- google_part/googletest.py
+++ google_part/googletest.py
@@ -8,35 +8,58 @@ import json
 import urllib
 import urllib2
 
+#def showsome(searchfor):
+#  query = urllib.urlencode({'q': searchfor})
+#  url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&%s' % query
+#  search_response = urllib.urlopen(url)
+#  search_results = search_response.read()
+#  results = json.loads(search_results)
+#  data = results['responseData']
+#  print 'Total results: %s' % data['cursor']['estimatedResultCount']
+#  hits = data['results']
+#  print 'Top %d hits:' % len(hits)
+#  for h in hits: print ' ', h['url']
+#  print 'For more results, see %s' % data['cursor']['moreResultsUrl']
+
+# showsome('katy perry')
+
+#output the dictionary of number of results matched with the artist from the list of artists
+
 def showsome(searchfor):
+
+
   query = urllib.urlencode({'q': searchfor})
+  print query
   url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&%s' % query
   search_response = urllib.urlopen(url)
   search_results = search_response.read()
+  print search_results
   results = json.loads(search_results)
   data = results['responseData']
-  print 'Total results: %s' % data['cursor']['estimatedResultCount']
+  #print 'Total results: %s' % data['cursor']['estimatedResultCount']
   hits = data['results']
-  print 'Top %d hits:' % len(hits)
+  #print 'Top %d hits:' % len(hits)
   for h in hits: print ' ', h['url']
-  print 'For more results, see %s' % data['cursor']['moreResultsUrl']
+  return results
+  #print 'For more results, see %s' % data['cursor']['moreResultsUrl']
 
-showsome('katy perry')
+#showsome('allintitle: foo fighters red hot chili peppers')
 
 #output the dictionary of number of results matched with the artist from the list of artists
 def processAll(init_artist, artists):
 
     artists_dic = dict()
     for first_artist in artists:
-        query = urllib.urlencode({'q': "allintitle: " + init_artist + first_artist})
-        url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&%s' % query
-        search_response = urllib.urlopen(url)
-        search_results = search_response.read()
-        #results = json.loads(search_results)
-        #data = results['responseData']
-        #length = 'Total results: %s' % data['cursor']['estimatedResultCount']
-        #hits = data['results']
-        artists_dic[first_artist] = len(search_results)
+        variable = "allintitle: " + init_artist + " " + first_artist
+        print variable
+
+        artists_dic[first_artist] = [showsome(variable)]
+
     return artists_dic
+
+#Tests
+
+print processAll("Mumford and Sons", ["Red Hot Chili Peppers", "linkin park", "bob dylan", "Taylor Swift"])
+
 
 
