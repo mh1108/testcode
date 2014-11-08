b771d5fbda59f705f95a6071c0958dc8c3cc9c31
denkerm <denker.m@husky.neu.edu>
2014-11-08 16:44:43 -0500

    We got the loop to work to create the correct dictionary, but we don't know how exactly to get the number of results.

--- google_part/googletest.py
+++ google_part/googletest.py
@@ -23,3 +23,20 @@ def showsome(searchfor):
 
 showsome('katy perry')
 
+#output the dictionary of number of results matched with the artist from the list of artists
+def processAll(init_artist, artists):
+
+    artists_dic = dict()
+    for first_artist in artists:
+        query = urllib.urlencode({'q': "allintitle: " + init_artist + first_artist})
+        url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&%s' % query
+        search_response = urllib.urlopen(url)
+        search_results = search_response.read()
+        #results = json.loads(search_results)
+        #data = results['responseData']
+        #length = 'Total results: %s' % data['cursor']['estimatedResultCount']
+        #hits = data['results']
+        artists_dic[first_artist] = len(search_results)
+    return artists_dic
+
+
