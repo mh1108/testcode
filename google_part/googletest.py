0501f6b7672028a1e4490e78d83afda1971cdb98
denkerm <denker.m@husky.neu.edu>
2014-11-08 21:54:25 -0500

    It works!

--- google_part/googletest.py
+++ google_part/googletest.py
@@ -1,47 +1,39 @@
-__author__ = 'misaki'
-
-
-
-
 #!/usr/bin/python
+__author__ = 'mdenker'
+
+import wikipedia
+import sys
+import spotipy
+from bs4 import BeautifulSoup
+
 import json
 import urllib
 import urllib2
 
-#def showsome(searchfor):
-#  query = urllib.urlencode({'q': searchfor})
-#  url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&%s' % query
-#  search_response = urllib.urlopen(url)
-#  search_results = search_response.read()
-#  results = json.loads(search_results)
-#  data = results['responseData']
-#  print 'Total results: %s' % data['cursor']['estimatedResultCount']
-#  hits = data['results']
-#  print 'Top %d hits:' % len(hits)
-#  for h in hits: print ' ', h['url']
-#  print 'For more results, see %s' % data['cursor']['moreResultsUrl']
-
-# showsome('katy perry')
-
-#output the dictionary of number of results matched with the artist from the list of artists
+    #init_artist = sys.argv[1]
+    #artists = sys.argv[2]
+    #artists_dic = dict[()]
 
 def showsome(searchfor):
 
-
   query = urllib.urlencode({'q': searchfor})
   print query
-  url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&%s' % query
-  search_response = urllib.urlopen(url)
+  opener = urllib2.build_opener()
+  opener.addheaders = [("User-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) "
+                                    "AppleWebKit/537.36 (KHTML, like Gecko) "
+                                    "Chrome/38.0.2125.111 Safari/537.36")]
+  url = 'https://www.google.com/search?%s' % query
+  search_response = opener.open(url)
   search_results = search_response.read()
-  print search_results
-  results = json.loads(search_results)
-  data = results['responseData']
-  #print 'Total results: %s' % data['cursor']['estimatedResultCount']
-  hits = data['results']
-  #print 'Top %d hits:' % len(hits)
-  for h in hits: print ' ', h['url']
-  return results
-  #print 'For more results, see %s' % data['cursor']['moreResultsUrl']
+  results = BeautifulSoup(search_results)
+  result_text = results.find(id="resultStats").get_text()
+  result_count = result_text.split()
+  if result_count[0] == 'About':
+      result_count = result_count[1]
+  result_count = result_count[0]
+
+  result_count = result_count.replace(',', '')
+  return int(result_count)
 
 #showsome('allintitle: foo fighters red hot chili peppers')
 
@@ -54,6 +46,12 @@ def processAll(init_artist, artists):
         print variable
 
         artists_dic[first_artist] = [showsome(variable)]
+
+    return artists_dic
+
+#Tests
+
+print processAll("Mumford and Sons", ["Red Hot Chili Peppers", "linkin park", "bob dylan", "Taylor Swift"])
 
     return artists_dic
 
